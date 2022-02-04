90457053bd2ec1f09ead02ee200704f6c78baf87
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-02-04 15:01:28 +0100

    done

--- form/post.php
+++ form/post.php
@@ -1,8 +1,12 @@
 <?php
-if($_POST['name'] || $_POST['email'] || ) {
+if($_POST['name'] || $_POST['email'] || $_POST['question'] || $_POST['message'] ) {
 
-    echo "Welcome" ." " .$_POST['name'];
-    echo "You are" ." " .$_POST['age'] ." " . "Years old.";
+    echo "Welcome :" ." " .$_POST['name'] ."<br />";
+    echo"<br />";
+    echo "Your email is :" ." " .$_POST['email'] ."<br />";
+    echo"<br />";
+    echo "Your question is :" ." " .$_POST['question'] ."<br />";
+    echo"<br />";
+    echo "You send the following message :" ." " .$_POST['message'] ."<br />";
 }
-
 ?>
