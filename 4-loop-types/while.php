a3fa4776bfd81769b9b001388d4b836a0dab8a7f
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-02-15 12:18:42 +0100

    done !

---
+++ 4-loop-types/while.php
@@ -0,0 +1,13 @@
+<?php
+$a = 0 ;
+$b = 6 ; 
+$i = 0 ;
+while($i<= 6 ){
+    $a++;
+    $b--;
+    $i++;
+}
+echo " first number a = " . $a , "  "  ."second number b = " .$b ;
+//   echo $a and $b;
+
+?>
