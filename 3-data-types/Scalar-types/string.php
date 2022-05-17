80ce14a2dae33d5947f0e9d7c6580e5befbecf90
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-17 12:20:53 +0100

    add string functions

--- 3-data-types/Scalar-types/string.php
+++ 3-data-types/Scalar-types/string.php
@@ -1,4 +1,20 @@
 <?php
 
-$name = 'Merradou';
+$name = 'MeRrAdoU';
 echo 'Hello ' . $name; // Hello Merradou
+// some string functions
+echo '<br>';
+
+echo strtolower($name);
+
+echo '<br>';
+
+echo strtoupper($name);
+
+echo '<br>';
+
+echo strlen($name);
+
+echo '<br>';
+
+echo $name[0];
