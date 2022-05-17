dd47821f05b52adf62a15a5bb4d1aaa9157da9dc
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-17 10:39:00 +0100

    arrays

---
+++ 3-data-types/Compound-types/arrays.php
@@ -0,0 +1,11 @@
+<?php
+// indexed arrays
+$fruits = array('banana', 'apple' , 'manga' );
+echo 'fruits : ' .$fruits[0] ."  "  .$fruits[1] ;
+// associative arrays
+$fruits = array('banana' => 'yellow',
+                'apple' => 'red' ,
+                 'manga' => 'green'
+              );
+echo 'fruits : ' .$fruits['banana'] ."  "  .$fruits['apple'];   
+?>
--- 3-data-types/arrays.php
+++
@@ -1,4 +0,0 @@
-<?php
-$fruits = array('banana', 'apple' , 'manga' );
-echo 'fruits : ' .$fruits[0] ."  "  .$fruits[1] ;
-?>
