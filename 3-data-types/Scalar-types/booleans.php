bad1257688a72ebe7ce2c06312969679554f7d8f
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-17 09:46:27 +0100

    scalar types booleans

---
+++ 3-data-types/Scalar-types/booleans.php
@@ -0,0 +1,13 @@
+<?php
+/* A variable is a scalar when it holds a single value of
+ the type integer, float, string, or boolean. */
+
+$true = true;
+
+$false = false;
+
+echo var_dump($true);
+echo var_dump($false);
+echo $true;
+
+?>
