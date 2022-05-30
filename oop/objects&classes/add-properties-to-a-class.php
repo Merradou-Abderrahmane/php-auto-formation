02e538b7b6ed6a798b607c1434ce2d59499f1afc
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-30 09:51:58 +0100

    add properties to a class

---
+++ oop/objects&classes/add-properties-to-a-class.php
@@ -0,0 +1,14 @@
+<?php
+
+class BankAccount
+{
+    public $accountNumber;
+    public $balance;
+}
+
+
+// set the values of the accountNumber and balance properties
+$account = new BankAccount();
+
+$account->accountNumber = 1;
+$account->balance = 100;
