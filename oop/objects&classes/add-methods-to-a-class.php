5b229b091e9e904cd7e8a718920ddae52fd07315
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-05-30 09:54:19 +0100

    add method

---
+++ oop/objects&classes/add-methods-to-a-class.php
@@ -0,0 +1,25 @@
+<?php
+
+class BankAccount
+{
+	public $accountNumber;
+
+	public $balance;
+
+	public function deposit($amount)
+	{
+		if ($amount > 0) {
+			$this->balance += $amount;
+		}
+	}
+}
+
+
+$account = new BankAccount();
+
+$account->accountNumber = 1;
+$account->balance = 100;
+
+$account->deposit(100);
+
+echo $account->balance; // 200
